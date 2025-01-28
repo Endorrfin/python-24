c4056381e3d7d0b6aacb5fb115b95c57082d2352
Krupka <krupka.ua@gmail.com>
2025-01-28 07:57:13 +0200

    python day 2

---
+++ complete_python_pro_bootcamp/Day 2/Mathematical Operations/task.py
@@ -0,0 +1,24 @@
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)
+print(5 / 3)
+print(5 // 3)
+print(2**2)
+print(2**3)
+
+print(3 * 3 + 3 / 3 -3)
+print(3 * (3 + 3) / 3 -3)
+
+height = 1.65
+weight = 84
+
+print(height ** 2)
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / (height ** 2)
+
+print(bmi)
