04ba2e80e57aa564611477203be95e1258de5b29
Vasyl Krupka <krupka.ua@gmail.com>
2025-02-09 17:10:25 +0200

    python day 6 (#8)

---
+++ complete_python_pro_bootcamp/Day 6/Functions/task.py
@@ -0,0 +1,18 @@
+print('Hello')
+num_char = len('Hello')
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+def my_function2():
+    print("2️⃣", "This will run")
+my_function2()
+
+def my_function3():
+    a = 3
+    if a > 2:
+        print("3️⃣", "This will run")
+my_function3()
