df98d00318859e6bc72c5a2c58f611846e00859e
Vasyl Krupka <krupka.ua@gmail.com>
2025-03-03 16:07:30 +0200

    python add coding exercises & readme file (#19)

---
+++ complete_python_pro_bootcamp/Day 13 Debugging/Coding Exercises/coding_12_odd_or_even.py
@@ -0,0 +1,21 @@
+# Debugging Odd or Even
+# - Read the code in exercise.py - Spot the problems 🐞.
+# - Modify the code to fix the program. Fix the code so that it works and passes the tests when you submit.
+
+# You can copy and paste the code into PyCharm to help you debug.
+# def odd_or_even(number):
+#     if number % 2 = 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+
+
+# ✅ SOLUTION
+number = int(input("Which number do you want to check?"))
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+odd_or_even(number)
