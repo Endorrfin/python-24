a2a8db31166edf41bf5114ff59f3bb521ef59350
Vasyl Krupka <krupka.ua@gmail.com>
2025-03-30 17:04:00 +0300

    python 17: Benefits of OOP | Quiz project (#21)

---
+++ complete_python_pro_bootcamp/Day 17 Benefits of OOP Quiz project/Brain quiz using trivia database/question_model.py
@@ -0,0 +1,7 @@
+class Question:
+
+    def __init__(self, q_text, q_answer, g_level, g_category):
+        self.text = q_text
+        self.answer = q_answer
+        self.level = g_level
+        self.category = g_category
