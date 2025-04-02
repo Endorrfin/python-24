95c6862897d86230b6501888abfd9855bffc2e43
Vasyl Krupka <krupka.ua@gmail.com>
2025-04-02 09:30:19 +0300

    python 18: Turtle The Graphical User Interface (GUI) (#22)

---
+++ complete_python_pro_bootcamp/Day 18 Turtle & Graphical User Interface (GUI)/draw-random-walk_using_named-colours.py
@@ -0,0 +1,21 @@
+import turtle as t
+from turtle import Screen
+import random
+
+tim = t.Turtle()
+
+
+# Challenge - draw random walk using named colors
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+
+screen = Screen()
+screen.exitonclick()
